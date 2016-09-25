2176705083b38c7ed224517e2b31eecf9a110081
钱梦颖 <qianmy1226@163.com>
2016-09-26 00:09:30 +0800

    3.0

--- generateCountryCodePlist/ViewController.swift
+++ generateCountryCodePlist/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var fountName = 0
+    var notFoundName = 0
+    var compareArray: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,94 +21,111 @@ class ViewController: UIViewController {
     
     func generate() {
         let codeDict = getDataFromJson()
+        print(codeDict.keys.count)
         let codeArray2 = addChinese()
-        
+        print(codeArray2.count)
         var CountryCodeArray = [[String: AnyObject]]()
-        var foundNum = 0
-        var notFoundNum = 0
         for (_, value) in codeDict {
             var dict = [String: AnyObject]()
             let country = value as! [String: AnyObject]
-            
             let dialling = country["dialling"] as! [String: AnyObject]
-            let diallingCode = dialling["calling_code"] as! [String]
-            let national_number_lengths = dialling["national_number_lengths"]
             
+            let diallingCode = dialling["calling_code"] as? [String]
+            let national_number_lengths = dialling["national_number_lengths"]
             let name = country["name"] as! [String: AnyObject]
             let common_name = name["common"] as! String
-            
-            var chinese_name = ""
-            
-            var name2 = ""
-//            for item in codeArray2 {
-//                let name = item["enName"]!
-//                if name == common_name {
-//                    name2 = name!
-//                    chinese_name = item["zhName"]!!
-//                    foundNum += 1
-//                    break
-//                }
-//            }
-//            if name2 == "" {
-//                print("\(common_name) not found")
-//                notFoundNum += 1
-//            }
-            
-            for item in codeArray2 {
-                let code = item["code"]!
-                if code == diallingCode.first! {
-                    name2 = code!
-                    chinese_name = item["zhName"]!!
-                    foundNum += 1
-                    break
+
+            if isValid(diallingCode as AnyObject?) && isValid(national_number_lengths) {
+                if diallingCode!.count > 0 {
+                    dict["calling_code"] = diallingCode as AnyObject?
+                    dict["common_name"] = common_name as AnyObject?
+                    dict["national_number_lengths"] = national_number_lengths
+                    let chinese_name = getChineseName(enName: common_name, diallingCode: diallingCode!.first!, codeArray2: codeArray2)
+                    dict["chinese_name"] = chinese_name as AnyObject?
+                    CountryCodeArray.append(dict)
                 }
             }
-            if name2 == "" {
-                print("\(common_name) not found")
-                notFoundNum += 1
-            }
-            
-            if isValid(diallingCode) && isValid(national_number_lengths) {
-                dict["calling_code"] = diallingCode
-                dict["common_name"] = common_name
-                dict["national_number_lengths"] = national_number_lengths
-                dict["chinese_name"] = chinese_name
-                CountryCodeArray.append(dict)
-            }
-//            else {
-//                print(common_name)
-//                print("null")
-//            }
-            
         }
+        print(fountName)
+        print(notFoundName)
+        print(CountryCodeArray.count)
+        print("=====")
+        for item in codeArray2 {
+            if let index = compareArray.index(of: item["zhName"]!!) {
+//                print(index)
+            } else {
+                print(item["zhName"])
+            }
+        }
+//        writeToPlist(fileArray: CountryCodeArray as NSArray)
         
-        print(foundNum)
-        print("============")
-        print(notFoundNum)
-        
-        let countryCodePlistPath = NSBundle.mainBundle().pathForResource("CountryCode.plist", ofType: nil)
-        print(countryCodePlistPath)
-        let fileArray = CountryCodeArray as NSArray
-        let result = fileArray.writeToFile(countryCodePlistPath!, atomically: true)
+//        let newArray = sortArray(array: CountryCodeArray)
+//        if let array = newArray as Array {
+//            writeToPlist(fileArray: array)
+//        } else {
+//            print("error")
+//        }
+        let a = sortArray(array: CountryCodeArray)
     }
     
-    func isValid(obj: AnyObject?) -> Bool {
+    func sortArray(array: [[String: AnyObject?]]) -> [[String: AnyObject?]]{
+        let arrayToSort = array as! NSArray
+        arrayToSort.sort
+        
+        let newArray = array.sorted { (item1, item2) -> Bool in
+            let name1 = item1["common_name"] as! String
+            let name2 = item2["common_name"] as! String
+            return name1 < name2
+        }
+        writeToPlist(fileArray: newArray as! NSArray)
+
+        
+        return newArray
+    }
+    
+    func getChineseName(enName: String, diallingCode: String ,codeArray2: [[String: String?]]) -> String {
+        var chinese_name = ""
+        var name2 = ""
+        for item in codeArray2 {
+            let code = item["code"]!
+            if code == diallingCode {
+                name2 = code!
+                chinese_name = item["zhName"]!!
+                compareArray.append(chinese_name)
+                fountName += 1
+                break
+            }
+        }
+        if name2 == "" {
+            notFoundName += 1
+            print("\(enName) not found")
+        }
+        return chinese_name
+    }
+    
+    func writeToPlist(fileArray: NSArray) {
+        let countryCodePlistPath = Bundle.main.path(forResource: "CountryCode.plist", ofType: nil)
+        print(countryCodePlistPath)
+        fileArray.write(toFile: countryCodePlistPath!, atomically: true)
+    }
+    
+    func isValid(_ obj: AnyObject?) -> Bool {
         if obj == nil {
             return false
         }
-        if obj!.isKindOfClass(NSNull) {
+        if obj! as! NSObject == NSNull() {
             return false
         }
         return true
     }
     
     func getDataFromJson() -> [String: AnyObject] {
-        let stringPath = NSBundle.mainBundle().pathForResource("Directions", ofType: "geojson")
-        let data = NSData(contentsOfFile: stringPath!)
+        let stringPath = Bundle.main.path(forResource: "Directions", ofType: "geojson")
+        let data = try? Data(contentsOf: URL(fileURLWithPath: stringPath!))
         var codeDict = [String: AnyObject]()
         
         do {
-            let dict = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
+            let dict = try JSONSerialization.jsonObject(with: data!, options: [])
             codeDict = dict as! [String: AnyObject]
         } catch {}
         return codeDict
@@ -113,31 +133,103 @@ class ViewController: UIViewController {
     
     func addChinese() -> [[String: String?]]{
 
-        let chinesePath = NSBundle.mainBundle().pathForResource("CountryWithChinese", ofType: "geojson")
-        let data = NSData(contentsOfFile: chinesePath!)
+        let chinesePath = Bundle.main.path(forResource: "CountryWithChinese", ofType: "geojson")
+        let data = try? Data(contentsOf: URL(fileURLWithPath: chinesePath!))
 
         var codeArray: [String] = []
         do {
-            let temp = try NSJSONSerialization.JSONObjectWithData(data!, options: [])
+            let temp = try JSONSerialization.jsonObject(with: data!, options: [])
             codeArray = temp as! [String]
         } catch {
             print("error")
         }
         var printArray = [[String: String?]]()
         for item in codeArray {
-            let array = item.componentsSeparatedByString(" ")
+            let array = item.components(separatedBy: " ")
             let count = array.count
             var enName = ""
             for i in 0..<(count - 2) {
-                enName.appendContentsOf(array[i])
+                enName.append(array[i])
             }
             let zhName = array[count - 2]
             let codeString = array.last!
-            let code = codeString.substringFromIndex(codeString.startIndex.advancedBy(1))
+            let code = codeString.substring(from: codeString.characters.index(codeString.startIndex, offsetBy: 1))
             let dict = ["enName": enName, "zhName": zhName, "code": code]
             printArray.append(dict)
         }
         return printArray
+    }
+    
+    func notFoundCountry() {
+        
+//        Optional(Optional("阿森松"))
+//        Optional(Optional("多米尼加共和国"))
+//        Optional(Optional("哈萨克斯坦"))
+//        Optional(Optional("吉尔吉斯坦"))
+//        Optional(Optional("荷属安的列斯"))
+//        Optional(Optional("东萨摩亚(美)"))
+//        Optional(Optional("圣文森特"))
+//        Optional(Optional("特立尼达和多巴哥"))
+//        Optional(Optional("美国"))
+        
+//        多米尼加联邦 DOMINICA(COMMOMWEALTHOF) 1767 -12
+//        多米尼加共和国 DOMINICAN REP 1809 -12
+        
+//        Eritrea not found
+//        Palau not found
+//        Saint Martin not found
+//        Sint Maarten not found
+//        Bosnia and Herzegovina not found
+//        Timor-Leste not found
+//        Marshall Islands not found
+//        Kiribati not found
+//        Uzbekistan not found
+//        Anguilla not found
+//        Guinea-Bissau not found
+//        Turks and Caicos Islands not found
+//        Vanuatu not found
+//        Niue not found
+//        Saint Pierre and Miquelon not found
+//        Croatia not found
+//        Norfolk Island not found
+//        Dominica not found
+//        Macedonia not found
+//        Curaçao not found
+//        Trinidad and Tobago not found
+//        Kazakhstan not found
+//        Aruba not found
+//        Palestine not found
+//        Grenada not found
+//        United States Virgin Islands not found
+//        Greenland not found
+//        Zambia not found
+//        Tokelau not found
+//        American Samoa not found
+//        Svalbard and Jan Mayen not found
+//        Saint Barthélemy not found
+//        Montenegro not found
+//        Vatican City not found
+//        Bhutan not found
+//        Kyrgyzstan not found
+//        Mauritania not found
+//        British Virgin Islands not found
+//        New Caledonia not found
+//        South Sudan not found
+//        Comoros not found
+//        Tuvalu not found
+//        Cape Verde not found
+//        British Indian Ocean Territory not found
+//        Guadeloupe not found
+//        Albania not found
+//        Ivory Coast not found
+//        Rwanda not found
+//        Falkland Islands not found
+//        Saint Kitts and Nevis not found
+//        Wallis and Futuna not found
+//        Equatorial Guinea not found
+//        South Georgia not found
+//        Micronesia not found
+//        Faroe Islands not found
     }
 
 }
